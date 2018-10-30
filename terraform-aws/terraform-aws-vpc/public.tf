482416041a86e51d0541aabf485a38d0377ef4fc
opsdev <44537342+aveeva-devops@users.noreply.github.com>
2018-10-29 22:28:13 -0400

    Update public.tf

--- terraform-aws/terraform-aws-vpc/public.tf
+++ terraform-aws/terraform-aws-vpc/public.tf
@@ -29,6 +29,24 @@ resource "aws_security_group" "web" {
         protocol = "icmp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
     egress { # SQL Server
         from_port = 1433
